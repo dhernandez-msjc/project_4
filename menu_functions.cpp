5f9e2073b7570b531947b9a5d7755c99eb533054
anaron91 <anaron385@student.msjc.edu>
2020-11-05 14:46:56 -0800

    create menu_functions .h/.ccp

---
+++ menu_functions.cpp
@@ -0,0 +1,6 @@
+//Author: Alex Naron
+//Github:
+//November 05, 2020
+//CSIS 113A
+
+#include "menu_functions.h"
---
+++ menu_functions.h
@@ -0,0 +1,11 @@
+//Author: Alex Naron
+//Github:
+//November 05, 2020
+//CSIS 113A
+
+#ifndef PROJECT_4__MENU_FUNCTIONS_H_
+#define PROJECT_4__MENU_FUNCTIONS_H_
+
+
+
+#endif //PROJECT_4__MENU_FUNCTIONS_H_
