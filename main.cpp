b0443fdc87a6cadc70a9f31d35f134356ebbe991
anaron91 <anaron385@student.msjc.edu>
2020-11-05 15:41:09 -0800

    display menu and get input

--- main.cpp
+++ main.cpp
@@ -4,8 +4,23 @@
 //CSIS 113A
 
 #include <iostream>
+#include "dragon_function.h"
+#include "menu_functions.h"
+#include <string>
+
+using std::cout;
+using std::endl;
 
 int main() {
+// initialize main loop
+
+  // display main menu
+  display_menu();
+
+  //get users menu choice
+  cout << "Enter menu choice: ";
+  char menu_choice;
+  std::cin >> menu_choice;
 
   return 0;
 }
