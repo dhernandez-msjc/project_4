e1c24ca36357063000863d1f82d57bb0952f8d24
anaron91 <anaron385@student.msjc.edu>
2020-11-05 14:45:24 -0800

    created dragon_functions and defined them

---
+++ dragon_function.cpp
@@ -0,0 +1,25 @@
+//Author: Alex Naron
+//Github:
+//November 05, 2020
+//CSIS 113A
+
+#include "dragon_function.h"
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+//define functions for dragon
+
+void bite() {
+cout << "Dragon bites you!" << endl;
+}
+
+void whip_tail(int damage) {
+cout << "Dragon whips tail for " << damage << " damage." << endl;
+}
+
+void breath_fire(int damage, int time) {
+cout << "Dragon breathes fire for " << time << " seconds, you take " << damage << " amount of damage." << endl;
+}
---
+++ dragon_function.h
@@ -0,0 +1,17 @@
+//Author: Alex Naron
+//Github:
+//November 05, 2020
+//CSIS 113A
+
+
+#ifndef PROJECT_4__DRAGON_FUNCTION_H_
+#define PROJECT_4__DRAGON_FUNCTION_H_
+
+// create functions for dragon
+
+void bite ();
+void whip_tail (int damage);
+void breath_fire (int damage, int time);
+
+
+#endif //PROJECT_4__DRAGON_FUNCTION_H_
